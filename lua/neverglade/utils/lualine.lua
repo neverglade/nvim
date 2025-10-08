893985918b847139bc232be85231cc8624c1fbde
Emma Jellema-Butler <emma.jellemabutler@gmail.com>
2025-10-08 18:07:48 +0200

    Finish lualine theme

--- lua/neverglade/utils/lualine.lua
+++ lua/neverglade/utils/lualine.lua
@@ -19,10 +19,34 @@ return function(variety)
 		c = { bg = C.heartwood, fg = C.text },
 	}
 
+	neverglade.visual = {
+		a = { bg = C.moss, fg = C.root, gui = "bold" },
+		b = { bg = C.bark, fg = C.text },
+		c = { bg = C.heartwood, fg = C.text },
+	}
+
+	neverglade.replace = {
+		a = { bg = C.rust, fg = C.root, gui = "bold" },
+		b = { bg = C.bark, fg = C.text },
+		c = { bg = C.heartwood, fg = C.text },
+	}
+
 	neverglade.command = {
 		a = { bg = C.aurora, fg = C.root, gui = "bold" },
 		b = { bg = C.bark, fg = C.text },
 		c = { bg = C.heartwood, fg = C.text },
+	}
+
+	neverglade.terminal = {
+		a = { bg = C.lavender, fg = C.root, gui = "bold" },
+		b = { bg = C.bark, fg = C.text },
+		c = { bg = C.heartwood, fg = C.text },
+	}
+
+	neverglade.inactive = {
+		a = { bg = C.root, fg = C.gray1, gui = "bold" },
+		b = { bg = C.root, fg = C.gray1 },
+		c = { bg = C.root, fg = C.gray1 },
 	}
 
 	return neverglade
