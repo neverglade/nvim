6a60545cae2b30f4ae3061aadd529012b5711d45
Emma Jellema-Butler <emma.jellemabutler@gmail.com>
2025-10-08 18:38:07 +0200

    Complete telescope integration

--- lua/neverglade/groups/integrations/telescope.lua
+++ lua/neverglade/groups/integrations/telescope.lua
@@ -4,6 +4,15 @@ local M = {}
 function M.get()
 	return {
 		TelescopeBorder = { link = "FloatBorder" },
+		TelescopeNormal = { link = "NormalFloat" },
+		TelescopePreviewNormal = { link = "TelescopeNormal" },
+		TelescopePromptNormal = { link = "TelescopeNormal" },
+		TelescopeResultsNormal = { link = "TelescopeNormal" },
+		TelescopeTitle = { link = "FloatTitle" },
+		TelescopeSelectionCaret = { fg = C.moss, bg = C.heartwood },
+		TelescopeSelection = { fg = C.lichen, bg = C.heartwood },
+		TelescopeMatching = { fg = C.aurora },
+		TelescopePromptPrefix = { fg = C.moss },
 	}
 end
 
